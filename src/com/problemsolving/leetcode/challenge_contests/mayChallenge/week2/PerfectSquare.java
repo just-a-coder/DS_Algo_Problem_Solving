d5a72d4a03890ea2972f895ebb1e2816d3108cc1
Just A Coder <54611515+just-a-coder@users.noreply.github.com>
2020-05-09 17:54:18 +0530

    refractoring

--- src/com/problemsolving/leetcode/challenge_contests/mayChallenge/week2/PerfectSquare.java
+++ src/com/problemsolving/leetcode/challenge_contests/mayChallenge/week2/PerfectSquare.java
@@ -19,23 +19,6 @@ public class PerfectSquare {
             return false;
     }
 
-/*    public static boolean isPerfectSquare(int num) {
-        if(num == 1) return true;
-        long start = 1, end = num/2, mid;
-        while (start <= end) {
-            mid = start + (end-start)/2;
-            long pow = mid * mid;
-            if (pow == num) {
-                return true;
-            } else if (pow > num) {
-                end = mid-1;
-            } else if (pow < num) {
-                start = mid+1;
-            }
-        }
-        return false;
-    }*/
-
     public static void main(String[] args) {
         int num = 808201;
         System.out.println(isPerfectSquare(num));
