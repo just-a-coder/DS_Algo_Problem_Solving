7fcae20dc39509bd4b97f50a216e7341cdb744a3
Just A Coder <54611515+just-a-coder@users.noreply.github.com>
2020-07-01 18:03:51 +0530

    first dp problem

---
+++ src/com/problemsolving/leetcode/challenge_contests/contests/BrowserHistory.java
@@ -0,0 +1,50 @@
+package com.problemsolving.leetcode.challenge_contests.contests;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+class BrowserHistory {
+
+    Stack<String> fstack;
+    Stack<String> bstack;
+    public BrowserHistory(String homepage) {
+        fstack = new Stack<>();
+        bstack = new Stack<>();
+        bstack.push(homepage);
+    }
+
+    public void visit(String url) {
+        bstack.push(url);
+        fstack = new Stack<>();
+    }
+
+    public String back(int steps) {
+        while(!bstack.isEmpty() && steps>0){
+            String elem = bstack.pop();
+            fstack.push(elem);
+            --steps;
+        }
+        return bstack.peek();
+    }
+
+    public String forward(int steps) {
+        while (!fstack.isEmpty() && steps > 0) {
+            String elem = fstack.pop();
+            bstack.push(elem);
+            --steps;
+        }
+        if (fstack.isEmpty()) {
+            return bstack.peek();
+        } else {
+            return fstack.peek();
+        }
+    }
+}
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
--- src/com/problemsolving/leetcode/challenge_contests/contests/Contest_Weekly.java
+++ src/com/problemsolving/leetcode/challenge_contests/contests/Contest_Weekly.java
@@ -1,22 +1,55 @@
 package com.problemsolving.leetcode.challenge_contests.contests;
 
+import com.problemsolving.leetcode.heap.medium.SortCharacterByFrequency_451;
+
 import java.util.*;
 
 public class Contest_Weekly {
 
-    public static boolean hasAllCodes(String s, int k) {
-        HashSet<String> binaryCodes = new HashSet<String>();
-        for(int i = 0; i + k <= s.length(); ++i) {
-            binaryCodes.add(s.substring(i, i+k));
+    public static int[] getStrongest(int[] arr, int k) {
+        int n = arr.length;
+        if(n == 1 && k == 1) return arr;
+        Arrays.sort(arr);
+        int medindex = (n-1)/2;
+        int median = arr[medindex];
+        int resarr[] = new int[k];
+        PriorityQueue<Pair> maxHeap = new PriorityQueue<>(new Pair());
+        for (int i = 0; i < n; i++) {
+            int diff = Math.abs(arr[i]-median);
+            Pair pr = new Pair(diff,arr[i]);
+            maxHeap.add(pr);
         }
 
-        double fullCount = Math.pow(2,k);
-        return binaryCodes.size() == fullCount;
+        int x = 0;
+        while (!maxHeap.isEmpty()) {
+            Pair pr = maxHeap.poll();
+            resarr[x++] = pr.elem;
+            if(x==k) return resarr;
+        }
+        return resarr;
     }
 
-    public static void main(String[] args) {
-        String str = "00110";
-        int k = 2;
-        System.out.println(hasAllCodes(str, k));
+    static class Pair implements Comparator<Pair> {
+        int freq;
+        int elem;
+        public Pair(){}
+        public Pair(int freq, int elem){
+            this.freq = freq;
+            this.elem = elem;
+        }
+
+        @Override
+        public int compare(Pair p1, Pair p2){
+            if (p2.freq == p1.freq) {
+                return p2.elem - p1.elem;
+            }else{
+                return p2.freq - p1.freq;
+            }
+        }
     }
-}
+
+        public static void main (String[]args){
+            int arr[] = new int[]{3,2,1,4,5};
+            System.out.println(Arrays.toString(getStrongest(arr, 2)));
+        }
+    }
