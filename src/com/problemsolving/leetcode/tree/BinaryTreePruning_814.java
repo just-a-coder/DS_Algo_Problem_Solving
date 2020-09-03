38b9dd144f557a95326eeff44baca0f858ee38ad
Just A Coder <54611515+just-a-coder@users.noreply.github.com>
2020-09-03 07:43:47 +0530

    tree pruning

---
+++ src/com/problemsolving/leetcode/tree/BinaryTreePruning_814.java
@@ -0,0 +1,14 @@
+package com.problemsolving.leetcode.tree;
+
+public class BinaryTreePruning_814 {
+    public TreeNode pruneTree(TreeNode root) {
+        if(root == null) return root;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        if(root.val == 0 && root.left == null && root.right == null){
+            return null;
+        }
+        return root;
+    }
+}
