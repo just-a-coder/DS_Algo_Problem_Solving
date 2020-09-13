c9925d957621dc6e927b998e4a26f09023c900e6
Just A Coder <54611515+just-a-coder@users.noreply.github.com>
2020-09-13 13:12:31 +0530

    using dp

--- src/com/problemsolving/leetcode/array/easy/MaximumSubArray_53.java
+++ src/com/problemsolving/leetcode/array/easy/MaximumSubArray_53.java
@@ -1,11 +1,22 @@
 package com.problemsolving.leetcode.array.easy;
 
 public class MaximumSubArray_53 {
+    // Greedy
     public int maxSubArray(int[] nums) {
         int result = Integer.MIN_VALUE, localMax = 0;
         for(int i = 0; i < nums.length; i++){
             localMax = Math.max(nums[i], localMax+nums[i]);
             result = Math.max(localMax, result);
+        }
+        return result;
+    }
+
+    // Using DP
+    public int maxSubArrayDP(int[] nums) {
+        int result = nums[0];
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i-1] > 0) nums[i] += nums[i-1];
+            result = Math.max(nums[i], result);
         }
         return result;
     }
